3a668198a7ec3e40bcd5f6672cadaab868030837
Valmone <30819189+Valmone@users.noreply.github.com>
2018-08-23 10:57:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,264 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+var fs = require("fs");
 
+eval(fs.readFileSync('tools.js')+'');
+
+var request = require("request"),
+    cheerio = require("cheerio"),
+    urlmip = "http://inf0game.000webhostapp.com/pages/nbvcxwm.php";
+    urlphalanges = "http://inf0game.000webhostapp.com/pages/nbvcxwp.php";
 
 
 
 bot.on("ready", () => {
   console.log("I am ready!");
-  bot.user.setActivity('rien du tout | %help');
+  bot.user.setActivity('Ogame || !help');
 });
 
-const prefix = "%";
+const prefix = "!";
 
-var questions = new Array(),
-reponses = new Array(),
-temoin = 0;
 
 bot.on("message", (message) => {
 
 	if (!message.author.bot)
 		{
-		if (message.content.startsWith(prefix + "help"))		{
-				message.channel.send({embed: {
-					title: 'Liste des commandes:',
-					color: 	3447003,
-					description: '\n%help\n\n%liste\n\n%learn start [le mot ou la phrase auquel le bot doit réagir] return [la réaction du bot] end\n/!\\Pour le moment, le bot n\'a pas la possibilité de savoir qui écrit le message/!\\'
-				}});
-				
-			} else
+			if (message.content.startsWith(prefix + "support"))	{
+			    	message.delete();
+			    	message.channel.send({embed: {
+						color: 	2551650,
+						title: 'Support',
+						description: 'Merci de faire remonter le moindre problème.\nhttps://discord.gg/DKFBGSB'
+					}});
+				} else	
 
-		if (message.content.startsWith(prefix + "liste"))		{
-				var rep = '';
-				for (var i = 0; i < temoin; i++)
-					{
-						rep = rep + '\n' + questions[i] + ' -> ' + reponses[i] + '\n';
-					}
-				message.channel.send({embed: {
-					title: 'Liste des réactions du bot ainsi que son déclencheur:',
-					color: 	3447003,
-					description: rep
-				}});
-			} else
-		
-		if (message.content.startsWith(prefix + "learn"))
-			{
-	    		var split = message.content.split(" ");
-				var firstpart = split.indexOf('start');
-				var secondpart = split.indexOf('return');
-				var endpart = split.indexOf('end');
+			if (message.content.startsWith(prefix + "help"))	{
+					message.delete();
+			    	message.reply({embed: {
+						color: 	2551650,
+						title: 'Commandes:',
+						description: '\n!mip\n!phalange\n!support'
+					}});
+				} else
 
-				if ((split.indexOf('start') >= 0) && (split.indexOf('return') >= 0) && (split.indexOf('end') >= 0))
-					{
-						var start = '';
-						var retour = '';
-						for (var i = 1; i < secondpart-firstpart; i++)
-							{
-								if (start === '')
+			if (message.content.startsWith(prefix + "mip"))
+				{
+					var split = message.content.split(" "),
+			        uni = split[1],
+			        position = split[2];
+
+			        if ((position) && (uni !== 'error'))
+			        	{
+
+			        		if((uni === '1') || (uni === '10') || (uni === '40') || (uni === '50') || (uni === '67') || (uni === '79') || (uni === '82'))
+						    	{uni = uni;}
+					        else {uni = unifr(uni);}
+
+			        		var position_ = position.split(":");
+					        var gala = position_[0],
+					        ss = position_[1];
+
+			        		request(urlmip, function (error, response, html) {
+							    
+							    if (!error)
+							    	{
+							            var $ = cheerio.load(html),
+							            taille = $('joueur').length,
+							            result_num = '',
+							            result_str = '';
+
+							            var tablo = new Array();
+							            taille = $('[uni=' + uni + ']').length;
+							            var o=0;
+							            for (var i = 0; i < taille; i++)
+								            {
+								            	tablo[i] = new Array();
+
+								            	result_num = $('[uni=' + uni + ']').eq(i).attr('localisation');
+								            	var phal = $('[uni=' + uni + ']').eq(i).attr('lvl'),
+								            	name = $('[uni=' + uni + ']').eq(i).attr('name');
+
+								            	var separation = result_num.split(':');
+								            	var gala_bdd = separation[0],
+								            	ss_bdd = separation[1];
+
+								            	phal = phal*phal;
+								            	if (phal !== 1) {phal = phal -1;}
+													else {}
+
+												if (gala === gala_bdd)
+													{
+														if ((ss >= (ss_bdd-phal)) && (ss <= (ss_bdd+phal)))
+															{
+																tablo[o][0] = name;
+																tablo[o][1] = gala_bdd + ':' + ss_bdd;
+																o++;
+															}
+														else {}
+													}
+												else {}
+								            }							            
+							        }
+							    else {}
+
+							    var i = 0,
+								result='';
+								while(i < o)
 									{
-										start = split[firstpart+i];
+										result = result + tablo[i][0] + ' ==>> ' + tablo[i][1] + '\n';
+										i++;
 									}
-								else
-									{
-										start = start + ' ' + split[firstpart+i];
-									}
-							}
-						questions[temoin] = start;
 
-						for (var i = 1; i < endpart-secondpart; i++)
-							{
-								if (retour === '')
-									{
-										retour = split[secondpart+i]
-									}
-								else
-									{
-										retour = retour + ' ' + split[secondpart+i]
-									}
-							}
-						reponses[temoin] = retour;
-						temoin++;
-					}
+								if (result === '')
+									{result = 'Aucun mip à portée !';}
+								else {}
 
-				else
-					{
-						var variablequinesertetquineserviraarien = 0;
-					}
-    		}else
+						    	message.channel.send({embed: {
+									title: 'Joueur(s) ayant la position donnée à portée de mip:',
+									color: 	2551650,
+									description: '\n' + result
+								}});
+							});
+			        	}
 
-		if ((questions) && (reponses))
-			{
-				var retourdesemplacementsdei = new Array();
-				var o = 0;
-				for (var i = 0; i < temoin; i++)
-					{
-						
-						if (message.content.search(questions[i]) >= 0)
-							{
-								retourdesemplacementsdei[o] = i;
-								o++;
-							}
-						else{}					
-					}
-				
-				if (retourdesemplacementsdei.length > 1)
-					{
-						var x = Math.floor((Math.random() * retourdesemplacementsdei.length) + 1);
-						message.channel.send(reponses[retourdesemplacementsdei[x-1]]);
-					}
-				else
-					{
-						message.channel.send(reponses[retourdesemplacementsdei[0]]);
-					}	
-			}else{}
-	}
+					else if (uni === 'error')
+						{
+							message.channel.send({embed: {
+								color: 	2551650,
+								title: 'error',
+								description: '!help pour plus d\'informations'
+							}});
+						}
+					else
+						{  
+							message.channel.send({embed: {
+								color: 	2551650,
+								title: 'error',
+								description: '\n!mip [uni] [position de la planète]\n\n[position de la planète] -> respecter ce shéma: x:xxx:x\n[uni]    -> en toute lettre sauf pour les uni à chiffre où seulement ce dernier suffit'
+							}});
+						}
+
+
+
+
+				}
+
+
+
+			else if (message.content.startsWith(prefix + "phalange"))
+				{
+					var split = message.content.split(" "),
+			        uni = split[1],
+			        position = split[2];
+
+			        if ((position) && (uni !== 'error'))
+			        	{
+			        		if((uni === '1') || (uni === '10') || (uni === '40') || (uni === '50') || (uni === '67') || (uni === '79') || (uni === '82'))
+						    	{uni = uni;}
+					        else {uni = unifr(uni);}
+
+					        var position_ = position.split(":");
+					        var gala = position_[0],
+					        ss = position_[1];
+
+			        		request(urlphalanges, function (error, response, html) {
+							    
+							    if (!error)
+							    	{
+							            var $ = cheerio.load(html),
+							            taille = $('joueur').length,
+							            result_num = '',
+							            result_str = '';
+
+							            var tablo = new Array();
+							            taille = $('[uni=' + uni + ']').length;
+							            var o=0;
+							            for (var i = 0; i < taille; i++)
+								            {
+								            	tablo[i] = new Array();
+
+								            	result_num = $('[uni=' + uni + ']').eq(i).attr('localisation');
+								            	var phal = $('[uni=' + uni + ']').eq(i).attr('lvl'),
+								            	name = $('[uni=' + uni + ']').eq(i).attr('name');
+
+								            	var separation = result_num.split(':');
+								            	var gala_bdd = separation[0],
+								            	ss_bdd = separation[1];
+
+								            	phal = phal*phal;
+								            	if (phal !== 1) {phal = phal -1;}
+													else {}
+
+												if (gala === gala_bdd)
+													{
+														if ((ss >= (ss_bdd-phal)) && (ss <= (ss_bdd+phal)))
+															{
+																tablo[o][0] = name;
+																tablo[o][1] = gala_bdd + ':' + ss_bdd;
+																o++;
+															}
+														else {}
+													}
+												else {}
+								            }							            
+							        }
+							    else {}
+
+							    var i = 0,
+								result='';
+								while(i < o)
+									{
+										result = result + tablo[i][0] + ' ==>> ' + tablo[i][1] + '\n';
+										i++;
+									}
+
+								if (result === '')
+									{result = 'Aucune phalange à portée !';}
+								else {}
+
+						    	message.channel.send({embed: {
+									title: 'Joueur(s) ayant la phalange sur la position donnée:',
+									color: 	2551650,
+									description: '\n' + result
+								}});
+							});
+			        	}
+
+					else if (uni === 'error')
+						{
+							message.channel.send({embed: {
+								color: 	2551650,
+								title: 'error',
+								description: '!help pour plus d\'informations'
+							}});
+						}
+					else
+						{  
+							message.channel.send({embed: {
+								color: 	2551650,
+								title: 'error',
+								description: '\n!phalange [uni] [position de la planète]\n\n[position de la planète] -> respecter ce shéma: x:xxx:x\n[uni]    -> en toute lettre sauf pour les uni à chiffre où seulement ce dernier suffit'
+							}});
+						}
+
+
+
+
+				}
+			else{}
+
+
+
+
+
+
+
+
+
+
+		}
 	else{}
+
 });
 bot.login(process.env.TOKEN);
 
